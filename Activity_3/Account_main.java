f2493e2db242692bdf23616259bc58eface14e69
mohammed radha <mohammedradha000@gmail.com>
2025-10-27 12:26:10 +0300

    Add files via upload

---
+++ Activity_3/Account_main.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Activity_3;
+
+/**
+ *
+ * @author DotNet
+ */
+public class Account_main {
+    
+}
